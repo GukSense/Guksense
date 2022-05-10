d043de8b18f2c2f24d718a6386c20f1b673ec114
강국 <@DESKTOP-LD79IDD>
2022-05-10 16:25:33 +0900

    hello world 메시지 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("Choi Dae Young");
 		System.out.println("He is Chiness");
+		System.out.println("Im hungry");
 	}
 }
