3c6e246987efd169186949aceadb85ec37a418f1
강국 <@DESKTOP-LD79IDD>
2022-05-10 15:44:01 +0900

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld {
+	public static void main(String[]args) {
+		System.out.println("Hello World");
+		System.out.println("Choi Dae Young");
+		System.out.println("He is Chiness");
+	}
+}
